ebb4634fb1e85e3009918981ddc650cd205d3ea5
isaiah dahl <isaiahdahl@me.com>
2017-10-31 16:21:14 +0000

    new rails app

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,7 @@
 class ArticlesController < ApplicationController
   before_action :article, only: [:edit, :show, :update, :destroy]
   def index
-    @tasks = Article.all
+    @articles = Article.all
   end
 
   def show
